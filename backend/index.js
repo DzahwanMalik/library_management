64999251358a6a29f1b6afe2362900afc66bb635
DzahwanMalik <malikdzahwan22@gmail.com>
2026-01-15 13:53:27 +0700

    fix: error module export

--- backend/index.js
+++ backend/index.js
@@ -22,22 +22,16 @@ app.use(bookRouter);
 app.use(userRouter);
 app.use(borrowedBookRouter);
 
-(async () => {
-  try {
-    await db.authenticate();
-    console.log("Database connected...");
+await db.authenticate();
+console.log("Database connected...");
 
-    await db.sync();
-    console.log("Database synced...");
+await db.sync();
+console.log("Database synced...");
 
-    app.get("/api", (req, res) => {
-      res.json({ message: "Hello from the backend :)" });
-    });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the backend :)" });
+});
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-})();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
